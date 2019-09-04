9e67564c1633d4125b4973218cbc7c9d3c02219a
alexeispirit <alexeispirit@gmail.com>
2019-09-04 14:42:44 +0500

    20-alert-state-and-component

---
+++ github-finder/src/components/layout/Alert.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "@fortawesome/fontawesome-free/css/all.min.css";
+
+const Alert = ({ alert }) => {
+  return (
+    alert !== null && (
+      <div className={`alert alert-${alert.type}`}>
+        <i className="fas fa-info-circle" /> {alert.msg}
+      </div>
+    )
+  );
+};
+
+export default Alert;
