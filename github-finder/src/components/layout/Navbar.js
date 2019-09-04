4d0ff4688a108ff8b2cf6876ba79047f4240c3a5
alexeispirit <alexeispirit@gmail.com>
2019-09-04 14:52:53 +0500

    21-react-router-setup

--- github-finder/src/components/layout/Navbar.js
+++ github-finder/src/components/layout/Navbar.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import PropTypes from "prop-types";
 import "@fortawesome/fontawesome-free/css/all.min.css";
 
@@ -9,6 +10,14 @@ const Navbar = ({ icon, title }) => {
         <i className={icon}></i>
         {title}
       </h1>
+      <ul>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/about">About</Link>
+        </li>
+      </ul>
     </nav>
   );
 };
