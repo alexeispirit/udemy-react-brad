2c212f58c3b8cf27721604f67cfed54559f77827
alexeispirit <alexeispirit@gmail.com>
2019-09-05 10:34:40 +0500

    33-alert-context-workflow

--- github-finder/src/components/layout/Alert.js
+++ github-finder/src/components/layout/Alert.js
@@ -1,7 +1,14 @@
-import React from "react";
+import React, { useContext } from "react";
+
+import AlertContext from "../../context/alert/alertContext";
+
 import "@fortawesome/fontawesome-free/css/all.min.css";
 
-const Alert = ({ alert }) => {
+const Alert = () => {
+  const alertContext = useContext(AlertContext);
+
+  const { alert } = alertContext;
+
   return (
     alert !== null && (
       <div className={`alert alert-${alert.type}`}>
