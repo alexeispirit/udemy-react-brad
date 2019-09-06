c8565b089108ac9cc195f98a93c492500ed4a817
alexeispirit <alexeispirit@gmail.com>
2019-09-06 14:32:38 +0500

    62-alert-context-state-and-component

--- contact-keeper/client/src/components/auth/Register.js
+++ contact-keeper/client/src/components/auth/Register.js
@@ -1,6 +1,11 @@
-import React, { useState } from "react";
+import React, { useState, useContext } from "react";
+
+import AlertContext from "../../context/alert/alertContext";
 
 const Register = () => {
+  const alertContext = useContext(AlertContext);
+  const { setAlert } = alertContext;
+
   const [user, setUser] = useState({
     name: "",
     email: "",
@@ -14,6 +19,13 @@ const Register = () => {
 
   const onSubmit = e => {
     e.preventDefault();
+    if (name === "" || email === "" || password === "") {
+      setAlert("Please enter all fields", "danger");
+    } else if (password !== password2) {
+      setAlert("Passwords do not match", "danger");
+    } else {
+      // submit
+    }
   };
 
   return (
@@ -30,6 +42,7 @@ const Register = () => {
             id="name"
             value={name}
             onChange={onChange}
+            required
           />
         </div>
         <div className="form-group">
@@ -40,6 +53,7 @@ const Register = () => {
             id="email"
             value={email}
             onChange={onChange}
+            required
           />
         </div>
         <div className="form-group">
@@ -50,6 +64,8 @@ const Register = () => {
             id="password"
             value={password}
             onChange={onChange}
+            required
+            minLength="6"
           />
         </div>
         <div className="form-group">
@@ -60,6 +76,8 @@ const Register = () => {
             id="password2"
             value={password2}
             onChange={onChange}
+            required
+            minLength="6"
           />
         </div>
         <input
