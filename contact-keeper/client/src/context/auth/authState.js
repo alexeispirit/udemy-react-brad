4f6f4c7fdcf5bbdddd0200cd3c2b566435901941
alexeispirit <alexeispirit@gmail.com>
2019-09-06 15:50:46 +0500

    66-logout-and-navbar

--- contact-keeper/client/src/context/auth/authState.js
+++ contact-keeper/client/src/context/auth/authState.js
@@ -96,7 +96,7 @@ const AuthState = props => {
   };
 
   // logout
-  const logout = () => {};
+  const logout = () => dispatch({ type: LOGOUT });
 
   // clear errors
   const clearErrors = () => {
